73048d86fa55d20f86e4dd570ccd57a033f1fe36
mvspavan1999 <pavan@campuscatalyst.in>
2025-05-03 10:19:51 +0530

    Source folder issue

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 #here we need to find a logic to find the destination dir. "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1"
-STORAGE_DIR = os.environ.get("STORAGE_DIR", "/Users/pavan/Downloads/docs")
+#/Users/pavan/Downloads/docs
+STORAGE_DIR = os.environ.get("STORAGE_DIR", "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1")
 
 
