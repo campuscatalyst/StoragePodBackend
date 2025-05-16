b741171b077dd49069621232bcffecdf4ea9255b
mvspavan1999 <pavan@campuscatalyst.in>
2025-05-16 21:44:12 +0530

    DB changes, Auth route'

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 #here we need to find a logic to find the destination dir. "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1"
 #/Users/pavan/Downloads/docs
-STORAGE_DIR = os.environ.get("STORAGE_DIR", "/Users/pavan/Downloads/docs")
+STORAGE_DIR = os.environ.get("STORAGE_DIR", "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1")
 
 class Settings(BaseSettings):
     SECRET_KEY: str
