ce74bd6d574b2b25e375682a0606e4079f90d9b0
mvspavan1999 <pavan@campuscatalyst.in>
2025-06-07 18:56:01 +0530

    getDestination in config

--- app/config.py
+++ app/config.py
@@ -6,8 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #here we need to find a logic to find the destination dir. "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1"
-#/Users/pavan/Downloads/docs
-STORAGE_DIR = os.environ.get("STORAGE_DIR", "/srv/dev-disk-by-uuid-09698ee9-3b6f-4504-b43d-d7b527129ac9/Folder1")
+
+def getDestinationFolder(base_path="/srv"):
+    for entry in os.listdir(base_path):
+        full_path = os.path.join(base_path, entry)
+        if os.path.isdir(full_path) and entry.startswith("dev-disk-"):
+            return f"${full_path}/Folder1"
+    return None
+
+STORAGE_DIR = os.environ.get("STORAGE_DIR", getDestinationFolder())
+print(STORAGE_DIR)
 
 JSON_DIR = "/root/scripts/json"
 METRICS_FILE = os.path.join(JSON_DIR, "metrics.json")
