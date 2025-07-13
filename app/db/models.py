9626fcd8480542faf52f583e6c9dae8a9336ec68
mvspavan1999 <pavan@campuscatalyst.in>
2025-07-13 18:02:25 +0530

    Enum fix

--- app/db/models.py
+++ app/db/models.py
@@ -1,19 +1,24 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional, Literal
 from datetime import datetime
+from enum import Enum
+
+class FileType(str, Enum):
+    file = "file"
+    folder = "folder"
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
     password: str
-    created_at: datetime = Field(default_factory=datetime.now())
-    updated_at: datetime = Field(default_factory=datetime.now())
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
 
 class FileEntry(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     path: str = Field(index=True)
-    type: Literal["file", "folder"] = Field(index=True)
+    type: FileType = Field(index=True)
     size: int
     modified_at: datetime
-    created_at: datetime = Field(default_factory=datetime.now())
+    created_at: datetime = Field(default_factory=datetime.now)
