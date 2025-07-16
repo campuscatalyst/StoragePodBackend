ce090ffb5d1152071766045f3752d9366d95b774
mvspavan1999 <pavan@campuscatalyst.in>
2025-07-16 18:44:18 +0530

    Logging db path

--- app/db/main.py
+++ app/db/main.py
@@ -1,10 +1,12 @@
 from sqlmodel import SQLModel, create_engine, Session
+from app.logger import logger
 
 DATABASE_URL = "sqlite:///./app/db/main.db"
 
 engine = create_engine(url=DATABASE_URL, connect_args={"check_same_thread": False})
 
 def init_db():
+    logger.info(f"Creating the db at - {DATABASE_URL}")
     SQLModel.metadata.create_all(engine)
 
 def get_session():
