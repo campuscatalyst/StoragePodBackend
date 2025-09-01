912f4bbf4c6c7bb1fd998f6509cdc8da434c7335
mvspavan1999 <pavan@campuscatalyst.in>
2025-09-01 11:59:21 +0530

    post upload tasks

--- app/api/routes/tus_server.py
+++ app/api/routes/tus_server.py
@@ -1,11 +1,40 @@
+import os, shutil
 from fastapi import HTTPException
 from tuspyserver.router import create_tus_router
 from app.config import TEMP_UPLOADS_DIR
+from app.core.file_manager import FileManager
+from app.logger import logger
 
 def on_upload_complete(file_path: str, metadata: dict):
-    print("Upload complete")
-    print(file_path)
-    print(metadata)
+    try:
+        target_dir = metadata.get("path")
+        filename = metadata.get("filename")
+
+        if not target_dir or not filename:
+            raise HTTPException(status_code=400, detail="Invalid metadata: path or filename missing")
+
+        abs_path = FileManager.validate_path(target_dir)
+
+        if not os.path.isdir(abs_path):
+            raise HTTPException(status_code=400, detail="Destination is not a directory")
+        
+        if not FileManager.validate_itemname(filename):
+            raise HTTPException(status_code=400, detail="Invalid filename provided")
+        
+        destination = os.path.join(target_dir, filename)
+
+        try:
+            # Move the file from temp to destination
+            shutil.move(file_path, destination)
+        except Exception as e:
+            logger.error(f"Failed to move file: {str(e)}")
+            raise
+        
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    except Exception as e:
+        logger.error(f"Error while uploading the file - {str(e)}")
 
 def pre_create_hook(metadata: dict, upload_info: dict):
     if "filename" not in metadata:
